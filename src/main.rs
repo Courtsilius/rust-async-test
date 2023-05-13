2ae3e71399a60345e0225afa7f5dd9d75c509bbb
Pascal <18620961+Courtsilius@users.noreply.github.com>
2023-05-13 19:23:23 +0200

    implement basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::fs::File;
+use clap::Parser;
+use rand::Rng;
+use std::io::Write;
+use std::ops::Add;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    n: u16,
+
+    #[arg(short, long)]
+    m: u16,
+
+    #[clap(long, short, action)]
+    read: bool,
+
+    #[clap(long, short, action)]
+    write: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("M: {}, N: {}", args.n, args.m);
+    if args.read {
+        read(args);
+    } else {
+        write(args);
+    }
+}
+
+fn read(args: Args) {
+    todo!();
+}
+
+fn write(args: Args) {
+    for n in 0..args.n {
+        let mut file_name = n.to_string().add(".txt");
+        write_file(&file_name, args.m)
+    }
+}
+
+fn write_file(file_name: &String, count: u16) {
+
+    let mut rng = rand::thread_rng();
+
+    let mut file = match File::options()
+        .create(true)
+        .append(true)
+        .open(file_name)
+    {
+        Ok(file) => {
+            file
+        }
+        Err(e) => {
+            std::process::exit(1)
+        }
+    };
+
+    for _ in 0..count {
+        let data = rng.gen::<u16>();
+        let output = serde_json::to_string(&data).unwrap();
+        writeln!(&mut file, "{}", output).unwrap();
+    }
 }
